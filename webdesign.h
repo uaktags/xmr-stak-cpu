4ac926d22d75d98d3a9d553e483c29313059e71b
Tim Garrity <timgarrity89@gmail.com>
2017-06-24 01:00:23 -0400

    Json web interface
    - Changed the content-type to json
    - Removed the CSS stuff
    - Basically it.

--- webdesign.h
+++ webdesign.h
@@ -1,19 +1,16 @@
 #pragma once
 
-extern const char sHtmlCssEtag[];
-extern const char sHtmlCssFile[];
-extern size_t sHtmlCssSize;
-
-extern const char sHtmlCommonHeader[];
-
 extern const char sHtmlHashrateBodyHigh[];
 extern const char sHtmlHashrateTableRow[];
+extern const char sHtmlHashrateTableRowComma[];
 extern const char sHtmlHashrateBodyLow[];
 
 extern const char sHtmlConnectionBodyHigh[];
 extern const char sHtmlConnectionTableRow[];
+extern const char sHtmlConnectionTableRowComma[];
 extern const char sHtmlConnectionBodyLow[];
 
 extern const char sHtmlResultBodyHigh[];
 extern const char sHtmlResultTableRow[];
+extern const char sHtmlResultTableRowComma[];
 extern const char sHtmlResultBodyLow[];
